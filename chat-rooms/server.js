f275f798773db87227a84b7f18f14d109296e2a6
Steven Rivers <sprivers@gmail.com>
2014-02-09 13:32:48 -0600

    added chat server lib

--- chat-rooms/server.js
+++ chat-rooms/server.js
@@ -56,5 +56,5 @@ server.listen(3000, function() {
   console.log("Server listening on port 3000.");
 });
 
-//var chatServer = require('./lib/chat_server');
-//chatServer.listen(server);
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
