eb4603ae017b5d4568fd6d1b73690281182ab032
Steven Rivers <sprivers@gmail.com>
2014-02-09 22:18:35 -0600

    more examples

---
+++ latest-posts/server.js
@@ -0,0 +1,35 @@
+var http = require('http');
+var fs = require('fs');
+
+
+http.createServer(function(req, res) {
+
+	if (req.url == '/') {
+
+		fs.readFile('./titles.json', function(err, data) {
+
+			if (err) {
+				console.error(err);
+				res.end('server error');
+			} else {
+
+				var titles = JSON.parse(data.toString());
+
+				fs.readFile('./template.html', function(err, data) {
+
+					if (err) {
+						console.error(err);
+						res.end('server error');
+					} else {
+
+						var templ = data.toString();
+
+						var html = templ.replace('%', titles.join('</li><li>'));
+						res.writeHead(200, {'Content-Type':'text/html'});
+						res.end(html);
+					}
+				});
+			}			
+		});
+	}
+}).listen(8000, '127.0.0.1');
