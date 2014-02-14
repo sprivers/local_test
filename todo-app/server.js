3bcbaf81cfd7522ee66cab95415001b0dd4f60ec
Steven Rivers <sprivers@gmail.com>
2014-02-13 22:15:44 -0600

    more examples

---
+++ todo-app/server.js
@@ -0,0 +1,38 @@
+var http = require('http');
+var url = require('url');
+
+var items = [];
+
+
+var server = http.createServer(function(req, res){
+
+	switch(req.method) {
+		case 'POST':
+			var item = '';
+			req.setEncoding('utf8');
+			req.on('data', function(chunk){
+				item += chunk;
+			});
+
+			req.on('end', function(){
+				items.push(item);
+				res.end('OK\n');
+			});
+			break;
+
+		case 'GET':
+			
+			var body = items.map(function(item, i){
+				return i + ') ' + item;
+			}).join('\n');
+
+			res.setHeader('Content-Length', Buffer.byteLength(body));
+			res.setHeader('Content-Type', 'text/plain; charset="utf-8"');
+			res.end(body + '\n');
+			break;
+	}
+});
+
+server.listen(3000, function() {
+  console.log("Server listening on port 3000.");
+});
