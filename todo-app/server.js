648d0847f30bbbd7a200b06cedf5febdbdfc13a1
Steven Rivers <sprivers@gmail.com>
2014-02-15 09:30:45 -0600

    todo app tutorial

--- todo-app/server.js
+++ todo-app/server.js
@@ -35,6 +35,21 @@ server.listen(3000, function() {
 
 function show(res) {
 
+	var html = '<html><head><title>Todo App</title></head><body>'
+			 + '<h1>Todo List</h1>'
+			 + '<ul>'
+			 + items.map(function(item) {
+			 		return '<li>' + item + '</li>'
+			 	}).join('')
+			 + '</ul>'
+			 + '<form method="post" action="/">'
+			 + '<p><input type="text" name="item" /></p>'
+			 + '<p><input type="submit" value="Add Item" /></p>'
+			 + '</form></body></html>';
+			 
+	res.setHeader('Content-Type', 'text/html');
+	res.setHeader('Content-Length', Buffer.byteLength(html));
+	res.end(html);
 }
 
 function addNew(req, res) {
@@ -46,6 +61,6 @@ function badRequest(res) {
 }
 
 function notFound(res) {
-	
+
 }
 
