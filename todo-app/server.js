b661721fdb76b3bdb6d883c248fbce665cbbc72e
Steven Rivers <sprivers@gmail.com>
2014-02-15 09:16:55 -0600

    rework todo app

--- todo-app/server.js
+++ todo-app/server.js
@@ -1,55 +1,51 @@
-var http = require('http');
-var url = require('url');
-
+var http  = require('http');
+var url   = require('url');
 var items = [];
 
 
 var server = http.createServer(function(req, res){
 
-	switch(req.method) {
-		case 'POST':
-			var item = '';
-			req.setEncoding('utf8');
-			req.on('data', function(chunk){
-				item += chunk;
-			});
+	if ('/' == req.url) {
 
-			req.on('end', function(){
-				items.push(item);
-				res.end('OK\n');
-			});
-			break;
-
-		case 'GET':
+		switch(req.method) {
 			
-			var body = items.map(function(item, i){
-				return i + ') ' + item;
-			}).join('\n');
+			case 'GET':
+				show(res);			
+				break;
+			
+			case 'POST':
+				addNew(req, res)
+				break;
+			
+			default:
+				badRequest(res);
+		}
 
-			body += '\n'
-			res.setHeader('Content-Length', Buffer.byteLength(body));
-			res.setHeader('Content-Type', 'text/plain; charset="utf-8"');
-			res.end(body);
-			break;
-
-		case 'DELETE':
-			var path = url.parse(req.url).pathname;
-			var i = parseInt(path.slice(1), 10);
-
-			if (isNaN(i)) {
-				res.statusCode = 400;
-				res.end('Invalid item id\n');
-			} else if (!items[i]) {
-				res.statusCode = 404;
-				res.end('Item not found\n');
-			} else {
-				items.splice(i, 1);
-				res.end('OK\n');
-			}
-			break;
+	} else {
+	
+		notFound(res);
+	
 	}
+
 });
 
 server.listen(3000, function() {
   console.log("Server listening on port 3000.");
 });
+
+function show(res) {
+
+}
+
+function addNew(req, res) {
+
+}
+
+function badRequest(res) {
+
+}
+
+function notFound(res) {
+	
+}
+
