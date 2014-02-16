b9a61b2b078c5885920f8c126a7552de58717ec2
Steven Rivers <sprivers@gmail.com>
2014-02-15 19:36:02 -0600

    more work on example upload

--- upload-fs/server.js
+++ upload-fs/server.js
@@ -1,4 +1,5 @@
-var http = require('http');
+var http 		= require('http');
+var formidable	= require('formidable');
 
 var server = http.createServer(function(req, res){
 	switch(req.method)
@@ -15,10 +16,50 @@ var server = http.createServer(function(req, res){
 
 function show(res) {
 	// show form
+	var html = '<html><title>Upload Server</title><body>'
+			 + '<form method="post" action="/" enctype="multipart/form-data">'
+			 + '<p><input type="text" name="name" /></p>'
+			 + '<p><input type="file" name="file" /></p>'
+			 + '<p><input type="submit" value="Upload" /></p>'
+			 + '</form>'
+			 + '</body></html>';
+
+	res.setHeader('Content-Type', 'text/html');
+	res.setHeader('Content-Length', Buffer.byteLength(html));
+	res.end(html);		 
 }
 
 function upload(req, res) {
 	// upload logic
+	if (!isFormData(req)) {
+		res.statusCode = 400;
+		res.end('Bad Request Type');
+		return;
+	}
+
+	var form = formidable.IncomingForm();
+
+	form.on('field', function(field, value) {
+		console.log(field);
+		console.log(value);
+	});
+
+
+	form.on('file', function(name, file) {
+		console.log(name);
+		console.log(file);
+	});
+
+	form.on('end', function() {
+		res.end('Upload Complete!\n');
+	});
+
+	form.parse(req);
+}
+
+function isFormData(req) {
+	var type = req.headers['content-type'] || '';
+	return 0 == type.indexOf('multipart/form-data');
 }
 
 
