534984cbf744b716ebb2690a1dd40a4735b514e4
Steven Rivers <sprivers@gmail.com>
2014-02-18 20:22:35 -0600

    continuation of time tracking example

--- timetracker/lib/timetrack.js
+++ timetracker/lib/timetrack.js
@@ -6,7 +6,7 @@ var qs = require('querystring');
 
 exports.sendHtml = function(res, html) {
 	res.setHeader('Content-Type', 'text/html');
-	res.setHeader('Content-Length', Buffer.byteLenght(html));
+	res.setHeader('Content-Length', Buffer.byteLength(html));
 	res.end(html);
 };
 
@@ -29,5 +29,106 @@ exports.actionForm = function(id, path, label) {
 };
 
 exports.add = function(db, req, res) {
-
+	exports.parseReceivedData(req, function(work) {
+		db.query(
+			"INSERT INTO work (hours, date, description) " +
+			" VALUES (?, ?, ?)", 
+			[work.hours, work.date, work.description], 
+			function(err) {
+				if (err) throw err;
+				exports.show(db, res);
+		});
+	});
 };
+
+exports.remove = function(db, req, res) {
+	exports.parseReceivedData(req, function(work) {
+		db.query(
+			"DELETE FROM work WHERE id=?",
+			[work.id],
+			function(err) {
+				if (err) throw err;
+				exports.show(db, res);
+			}
+		);
+	});
+};
+
+exports.archive = function(db, req, res) {
+	exports.parseReceivedData(req, function(work) {
+		db.query(
+			"UPDATE work SET archived=1 WHERE id=?",
+			[work.id],
+			function(err) {
+				if (err) throw err;
+				exports.show(db, res);
+			}
+		);
+	});
+};
+
+exports.show = function(db, res, showArchived) {
+	var sql = "SELECT * FROM work " +
+		"WHERE archived=? " +
+		"ORDER BY date DESC";
+
+	var archivedValue = (showArchived) ? 1 : 0;
+	
+	db.query(sql, [archivedValue], 
+		function(err, rows) {
+			if (err) throw err;
+
+			html = (showArchived) 
+				? '' : '<a href="/archived">Archived Work</a><br/>';
+
+			html += exports.workHitlistHtml(rows);
+			html += exports.workFormHtml();
+			exports.sendHtml(res, html);	
+		});	
+};
+
+exports.showArchived = function(db, res) {
+	exports.show(db, res, true);
+};
+
+exports.workHitlistHtml = function(rows) {
+	var html = '<table>';
+
+	for (var i in rows) {
+		html += '<tr>';
+		html += '<td>' + rows[i].date + '</td>';
+		html += '<td>' + rows[i].hours + '</td>';
+		html += '<td>' + rows[i].description + '</td>';
+
+		if (!rows[i].archived) {
+			html += '<td>' + exports.workArchiveForm(rows[i].id) + '</td>';
+		}
+
+		html += '<td>' + exports.workDeleteForm(rows[i].id) + '</td>';
+		html += '</tr>'
+	}
+	html   += '</table>';
+};
+
+exports.workFormHtml = function() {
+	var html = '<form method="POST" action ="/">' +
+	'<p>Date (YYY-MM-DD):<br/><input name="date" type="text"><p/>' +
+	'<p>Hours worked:<br/><input name="hours" type="text"><p/>' +
+	'<p>Description:<br/>' +
+	'<textarea name="description"></textarea></p>' +
+	'<input type="submit" value="Add" />'
+	'</form>';
+	return html;
+};
+
+
+exports.workArchiveForm = function(id) {
+	return exports.actionForm(id, '/archive', 'Archive');
+};
+
+exports.workDeleteForm = function(id) {
+	return exports.actionForm(id, '/delete', 'Delete');
+};
+
+
+
