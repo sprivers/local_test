ba199b8b7edaf86929d15cd1279dfa8118d5fccf
Steven Rivers <sprivers@gmail.com>
2014-02-20 16:10:58 -0600

    starting connect middleware examples

--- timetracker/timetrack_server.js
+++ timetracker/timetrack_server.js
@@ -1,4 +1,3 @@
-
 var http  = require('http');
 var work  = require('./lib/timetrack');
 var mysql = require('mysql');
@@ -14,36 +13,29 @@ var db = mysql.createConnection({
 var server = http.createServer(function(req, res) {
 
 	switch (req.method) {
-
 		case 'POST':
 			switch(req.url) {
 				case '/':
 					work.add(db, req, res);
 					break;
-
 				case '/archive':
 					work.archive(db, req, res);
 					break;
-
 				case '/delete':
 					work.delete(db, req, res);
 					break;
 			}
 			break;
-
 		case 'GET':
 			switch (req.url) {
 				case '/':
 					work.show(db, res);
 					break;
-
 				case '/archived':
 					work.showArchived(db, res);
 			}
 			break;
-
 	}
-
 });
 
 db.query(
@@ -60,4 +52,3 @@ db.query(
 		server.listen(3000, '127.0.0.1');
 	}
 );
-
