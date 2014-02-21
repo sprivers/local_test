8efc7eb84b211ad12d98604f614f868972789365
Steven Rivers <sprivers@gmail.com>
2014-02-20 19:17:35 -0600

    added ws version of photo app

---
+++ photo/routes/photos.js
@@ -0,0 +1,4 @@
+var photos = [];
+
+
+photos.push()
---
+++ ws_photo/routes/photos.js
@@ -0,0 +1,11 @@
+var photos = [];
+
+photos.push({
+    name: 'Node.js logo',
+    path: 'http://nodejs.org/images/logos/nodejs-green.png'
+});
+
+photos.push({
+    name: 'Ryan Speaking',
+    path: 'http://nodejs.org/images/ryan-speaker.jpg'
+});
